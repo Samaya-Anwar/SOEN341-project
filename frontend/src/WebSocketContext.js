1fd456484783fc76252a112229dbbacbf4de0980
Salima Ammar <selimaammar07@gmail.com>
2025-02-25 18:16:41 -0500

    integrate websockets (frontend)

---
+++ frontend/src/WebSocketContext.js
@@ -0,0 +1,31 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+const WebSocketContext = createContext(null);
+
+export const useWebSocket = () => useContext(WebSocketContext);
+
+export const WebSocketProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:5000");
+    ws.onopen = () => {
+      console.log("Connected to ws server");
+    };
+    ws.onclose = () => {
+      console.log("Disconnected from ws server");
+    };
+    ws.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+    setSocket(ws);
+    return () => {
+      ws.close();
+    };
+  }, []);
+  return (
+    <WebSocketContext.Provider value={socket}>
+      {children}
+    </WebSocketContext.Provider>
+  );
+};
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { WebSocketProvider } from "./WebSocketContext";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <WebSocketProvider>
+      <App />
+    </WebSocketProvider>
   </React.StrictMode>
 );
 
