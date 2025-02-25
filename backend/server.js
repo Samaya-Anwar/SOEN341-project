afcbb069df4c23781d62d20ff4bdf124e14c7dab
Salima Ammar <selimaammar07@gmail.com>
2025-02-25 18:03:39 -0500

    integrate websockets (backend)

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const http = require("http");
+const WebSocket = require("ws");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ port: 8080 });
 
-app.use(bodyParser.json());
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+ws.on("message", (message) => {
+  console.log("received: %s", message);
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+});
+ws.on("close", () => {
+  console.log("Client disconnected");
+});
+const PORT = process.env.PORT || 5050;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
