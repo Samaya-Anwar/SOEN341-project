9b9233a6b82b84d62379c7df509bdd0e757ea049
Beryl-beep <beryl.liang01@gmail.com>
2025-03-05 11:33:21 -0500

    Added Google Login Option

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GoogleOAuthProvider clientId="198072547047-vtauj833icvolq5cs13i4ted4gs9a6d8.apps.googleusercontent.com">
     <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
